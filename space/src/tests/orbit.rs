1c2d41d28cc28aa4dd6a6bf1cf12b18e6c9dbc76
Matthew Cornell <mcornell400708@gmail.com>
2023-10-11 20:56:08 -0400

    Orbital predictions and better unit tests

--- space/src/tests/orbit.rs
+++ space/src/tests/orbit.rs
@@ -1,5 +1,8 @@
 use crate::body::*;
+use bnum::cast::*;
 use std::f64::consts::PI;
+use std::fmt::Display;
+use std::ops::Sub;
 #[allow(dead_code)]
 fn round(val: f64, digits: u8) -> f64 {
     let exp = 10u32.pow(digits as _) as f64;
@@ -10,36 +13,71 @@ fn trunc(val: u128, digits: u8) -> u128 {
     let exp = 10u128.pow(digits as _);
     val / exp * exp
 }
+#[inline(always)]
+fn assert_err<T: As + Sub<Output = T> + PartialOrd + Copy, F: Display>(
+    calculated: T,
+    expected: impl Into<T>,
+    error: f64,
+    msg: F,
+) where
+    f64: CastFrom<T>,
+{
+    let expected = expected.into();
+    let err = (if calculated > expected {
+        calculated - expected
+    } else {
+        expected - calculated
+    })
+    .as_::<f64>()
+        / expected.as_::<f64>();
+    if err.abs() > error {
+        panic!(
+            "{msg}: error is {:.4}%, maximum allowed is {:.4}%",
+            err * 100.0,
+            error * 100.0
+        );
+    }
+}
 #[test]
 fn earth_sun() {
     // numbers taken from https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
     let sol = UInt::from(19885u64) * UInt::TEN.pow(26);
     let o = Orbit::EARTH;
-    assert_eq!(
-        o.perihelion() / 10u64.pow(9),
-        UInt::from(147u64),
-        "perihelion failed"
+    assert_err(
+        o.perihelion(),
+        147095000000u64,
+        0.00028,
+        "perihelion failed",
     );
-    assert_eq!(
-        o.aphelion() / 10u64.pow(9),
-        UInt::from(152u64),
-        "aphelion failed"
+    assert_err(o.aphelion(), 152100000000u64, 0.00028, "aphelion failed");
+    assert_err(
+        o.orbital_distance(0.0),
+        o.perihelion(),
+        0.00001,
+        "orbital distance @ perihelion failed",
     );
-    assert_eq!(
-        o.orbital_distance(0.0) / 10u64.pow(6),
-        o.perihelion() / 10u64.pow(6),
-        "orbital distance @ perihelion failed"
+    assert_err(
+        o.orbital_distance(PI),
+        o.aphelion(),
+        0.00001,
+        "orbital distance @ aphelion failed",
     );
-    assert_eq!(
-        o.orbital_distance(PI) / 10u64.pow(6),
-        o.aphelion() / 10u64.pow(6),
-        "orbital distance @ aphelion failed"
+    assert_err(
+        o.orbital_period(sol),
+        31558809u64,
+        0.0001,
+        "orbital period failed",
     );
-    assert_eq!(
-        o.orbital_period(sol) / 10u64.pow(4),
-        UInt::from(31558809u64) / 10u64.pow(4),
-        "orbital period failed"
-    );
+    let per = o.orbital_period(sol);
+    for _ in 0..100 {
+        let t = rand::random();
+        assert_err(
+            o.predict(sol, t, per),
+            t,
+            0.0001,
+            format_args!("orbital predictions failed with starting angle of {t:.6} radians"),
+        );
+    }
 }
 
 #[test]
@@ -47,29 +85,34 @@ fn earth_moon() {
     // numbers taken from https://nssdc.gsfc.nasa.gov/planetary/factsheet/moonfact.html
     let sol = UInt::from(5972u64) * UInt::TEN.pow(21);
     let o = Orbit::MOON;
-    assert_eq!(
-        o.perihelion() / 10u64.pow(6),
-        UInt::from(363u64),
-        "perihelion failed"
+    assert_err(o.perihelion(), 363300000u64, 0.00001, "perihelion failed");
+    assert_err(o.aphelion(), 405500000u64, 0.00001, "aphelion failed");
+    assert_err(
+        o.orbital_distance(0.0),
+        o.perihelion(),
+        0.0,
+        "orbital distance @ perihelion failed",
     );
-    assert_eq!(
-        o.aphelion() / 10u64.pow(6),
-        UInt::from(405u64),
-        "aphelion failed"
+    assert_err(
+        o.orbital_distance(PI),
+        o.aphelion(),
+        0.0,
+        "orbital distance @ aphelion failed",
     );
-    assert_eq!(
-        o.orbital_distance(0.0) / 10u64.pow(3),
-        o.perihelion() / 10u64.pow(3),
-        "orbital distance @ perihelion failed"
+    assert_err(
+        o.orbital_period(sol),
+        2360594u64,
+        0.005,
+        "orbital period failed",
     );
-    assert_eq!(
-        o.orbital_distance(PI) / 10u64.pow(3),
-        o.aphelion() / 10u64.pow(3),
-        "orbital distance @ aphelion failed"
-    );
-    assert_eq!(
-        o.orbital_period(sol) / 10u64.pow(4),
-        UInt::from(237u64),
-        "orbital period failed"
-    );
+    let per = o.orbital_period(sol);
+    for _ in 0..100 {
+        let t = rand::random();
+        assert_err(
+            o.predict(sol, t, per),
+            t,
+            0.0001,
+            format_args!("orbital predictions failed with starting angle of {t:.6} radians"),
+        );
+    }
 }
